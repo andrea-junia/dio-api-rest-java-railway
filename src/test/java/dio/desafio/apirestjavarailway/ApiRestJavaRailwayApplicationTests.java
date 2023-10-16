de7e6cdba523efc5ff15a850b838e16ec80b0b05
andrea-junia <andre3aj.f@gmail.com>
2023-10-16 18:59:16 -0300

    first commit

---
+++ src/test/java/dio/desafio/apirestjavarailway/ApiRestJavaRailwayApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.desafio.apirestjavarailway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestJavaRailwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
